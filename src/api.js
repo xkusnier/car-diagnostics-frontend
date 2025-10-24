68daf94b86887a7cf8fe2df1431e5bf42ca3f9f8
iubou <114062398+xkusnier@users.noreply.github.com>
2025-10-25 00:11:46 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 export const api = axios.create({
- baseurl = "https://car-diagnostics.onrender.com";, // Nahraďte správnym URL backendu
+  baseURL: "https://car-diagnostics.onrender.com", // Nahraďte správnym URL backendu
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("jwt_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => Promise.reject(error));
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("jwt_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
