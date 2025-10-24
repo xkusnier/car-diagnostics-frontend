f018f1cde556721a429f393e65a1755b154f5c72
iubou <114062398+xkusnier@users.noreply.github.com>
2025-10-25 01:07:54 +0200

    Create MainScreen component with basic structure

---
+++ src/MainScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainScreen() {
+  return (
+    <div style={{ backgroundColor: "white", minHeight: "100vh", width: "100%" }}>
+      {/* Zatiaľ žiadny obsah */}
+    </div>
+  );
+}
+
+export default MainScreen;
