ca6207c4dd5a092f7148c5c84deba2296335fc48
iubou <114062398+xkusnier@users.noreply.github.com>
2026-02-17 18:47:02 +0100

    Update LiveDataScreen.js

--- src/LiveDataScreen.js
+++ src/LiveDataScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { api } from "./api";
+import { io } from "socket.io-client";
 import "./styles/global.css";
 
 function LiveDataScreen({ deviceId, onBack, deviceInfo }) {
