2fbb5dfcd308aa94bd35148bebe5976b36658d15
iubou <114062398+xkusnier@users.noreply.github.com>
2026-02-17 16:42:31 +0100

    Replace VehicleTelemetryComparison CSS import

--- src/VehicleTelemetryComparison.js
+++ src/VehicleTelemetryComparison.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { api } from "../api";
-import "./styles/VehicleTelemetryComparison.css";
+import "./styles/global.css";
 
 function VehicleTelemetryComparison({ onNavigate }) {
   const [vehicles, setVehicles] = useState([]);
